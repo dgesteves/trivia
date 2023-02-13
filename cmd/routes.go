cb53e7dbfc4957fa410a4d57db6a518acb09b370
dgesteves <diogo.esteves.goncalves@gmail.com>
2023-02-13 15:30:28 +0000

    feat(project): routes.go |X| Introduces routes file

---
+++ cmd/routes.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/dgesteves/trivia/handlers"
+	"github.com/gofiber/fiber/v2"
+)
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/", handlers.ListFacts)
+
+	app.Post("/fact", handlers.CreateFact)
+}
